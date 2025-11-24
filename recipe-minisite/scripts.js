12a158b4ab1181c6c480b49ee6aa75fb165dd623
rrosee-p <rose.pineda@wsu.edu>
2025-11-23 19:17:57 -0800

    i hate this

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,14 +1,12 @@
-// Responsive menu toggle
+// JS for responsive menu
 document.addEventListener("DOMContentLoaded", function () {
     const navBurger = document.getElementById('navBurger');
     const nav = document.getElementById('mainNav');
 
-    // Toggle menu on burger click
     navBurger.addEventListener('click', () => {
         nav.classList.toggle('show-menu');
     });
 
-    // Optional: close menu when clicking outside nav
     document.addEventListener('click', function (e) {
         if (!e.target.closest('nav') && nav.classList.contains('show-menu')) {
             nav.classList.remove('show-menu');
