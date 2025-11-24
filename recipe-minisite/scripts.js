8dc92ce84e7de748cf76f0d12138bc77f6dc65b3
rrosee-p <rose.pineda@wsu.edu>
2025-11-23 18:42:01 -0800

    th

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,10 +1,19 @@
-// Handle dropdowns for both sections
-const dropdownLinks = document.querySelectorAll(".drop-link");
+// Toggle the mobile menu
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("navMenu");
 
-dropdownLinks.forEach(link => {
-    link.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent page jump
-        const parent = this.parentElement;
-        parent.classList.toggle("open");
+btn.onclick = function () {
+    menu.classList.toggle("open");
+};
+
+
+// Dropdown toggle for "Recipes" and "Recipe Websites"
+const dropToggles = document.querySelectorAll(".drop-toggle");
+
+dropToggles.forEach(toggle => {
+    toggle.addEventListener("click", function (event) {
+        event.preventDefault(); // stop page jump
+        const section = this.parentElement; // li element
+        section.classList.toggle("open");   // open/close dropdown
     });
 });
