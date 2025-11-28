626450f676ad99b5b1aa53f485e93d53f5bbc199
rrosee-p <rose.pineda@wsu.edu>
2025-11-28 15:07:56 -0800

    .

---
+++ Major-Project/scripts.js
@@ -0,0 +1,16 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach(slide => slide.style.display = "none");
+    slides[i].style.display = "block";
+}
+
+function nextSlide() {
+    index++;
+    if (index >= slides.length) index = 0;
+    showSlide(index);
+}
+
+showSlide(index);
+setInterval(nextSlide, 4000); // Change slide every 4 seconds
